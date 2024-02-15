6ea67a185fb3a685f2f6ca17c40c67991890c42f
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2024-02-15 19:36:56 +0800

    修正 docker-SHELL.sh
    怎么if都能写错阿喂
    顺便把运行参数变得更 --human 一点

--- docker-SHELL.sh
+++ docker-SHELL.sh
@@ -1,13 +1,17 @@
 #!/bin/env bash
 
 # 如果参数非空则使用输入作为启动命令
-if [[ -n "$@" ]]
-{
+if [[ -n "$@" ]]; then
     Command="$@"
-} else {
+else
     # 否则使用镜像缺省参数:
     # CMD [ "bundle", "exec", "jekyll", "server", "--host", "0.0.0.0", "--port", "4000" ]
     Command=""
-}
+fi
 
-docker run --rm -it -v ./:/site --name sourlemonjuiceblog sourlemonjuiceblog:latest $Command
+docker run --rm --interactive --tty \
+--volume "./:/site" \
+--publish "4000:4000" \
+--name sourlemonjuiceblog \
+sourlemonjuiceblog:latest \
+$Command
