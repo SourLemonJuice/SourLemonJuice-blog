2d7520a5fc172f95396d1b6e55f9f24e01e44abb
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-01-19 03:20:07 +0800

    添加用来快速创建帖子的 newpost.sh 脚本
    写的应该还是很防呆的吧（小声

---
+++ newpost.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ ! -f "./_config.yml" ]]; then
+    echo "This script needs to be run from the repository root"
+    exit
+fi
+
+if [[ -z "$1" ]]; then
+    post_id="post"
+else
+    post_id="$1"
+fi
+
+base_path="posts2/$(date +%Y)/_posts"
+mkdir --parents "${base_path}"
+
+post_path="${base_path}/$(date +%Y-%m-%d)-${post_id}.md"
+read -p "Press enter to create: ${post_path}"
+
+if [[ -f "${post_path}" ]]; then
+    read -p "File already exists, override?(input ignore, ctrl-c to cancel)"
+fi
+
+cat <<EOF > "${post_path}"
+---
+lang: "zho-Hans"
+title: "${post_id}"
+date: $(date "+%Y-%m-%d %R %z")
+---
+EOF
